5f5ec1ad6cedc9c6b3bb8547c03a31e9e0a39769
Oguz Gelal <o.gelal77@gmail.com>
2019-12-15 14:50:44 -0500

    session counter

--- functions/index.js
+++ functions/index.js
@@ -5,40 +5,35 @@ const admin = require('firebase-admin');
 admin.initializeApp();
 
 // Keeps track of the length of the 'likes' child list in a separate property.
-exports.countsessionchange = functions.database
+exports.countSessionChange = functions.database
   .ref('/users/{userid}/sessions/{sessionid}')
   .onWrite(async (change) => {
-      const collectionRef = change.after.ref.parent;
-      const countRef = collectionRef.parent.child('session_count');
+    const collectionRef = change.after.ref.parent;
+    const countRef = collectionRef.parent.child('session_count');
 
-      let increment;
-      if (change.after.exists() && !change.before.exists()) {
-        increment = 1;
-      } else if (!change.after.exists() && change.before.exists()) {
-        increment = -1;
-      } else {
-        return null;
-      }
-
-      // Return the promise from countRef.transaction() so our function
-      // waits for this async event to complete before it exits.
-      await countRef.transaction((current) => {
-        return (current || 0) + increment;
-      });
-
-      console.log('Counter updated.');
+    let increment;
+    if (change.after.exists() && !change.before.exists()) {
+      increment = 1;
+    } else if (!change.after.exists() && change.before.exists()) {
+      increment = -1;
+    } else {
       return null;
+    }
+
+    await countRef.transaction((current) => {
+      return (current || 0) + increment;
     });
 
-// If the number of likes gets deleted, recount the number of likes
-exports.recountsessions = functions.database
+    return null;
+  });
+
+// If the number of sessions gets deleted, recount
+exports.recountSessions = functions.database
   .ref('/users/{userid}/session_count')
   .onDelete(async (snap) => {
     const counterRef = snap.ref;
     const collectionRef = counterRef.parent.child('sessions');
 
-    // Return the promise from counterRef.set() so our function
-    // waits for this async event to complete before it exits.
     const messagesData = await collectionRef.once('value');
     return await counterRef.set(messagesData.numChildren());
 });
