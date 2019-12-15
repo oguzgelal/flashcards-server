0fbe7f1a62045a623c4b181cf98052416007f303
Oguz Gelal <o.gelal77@gmail.com>
2019-12-15 15:04:48 -0500

    typo

--- functions/src/sessionCounter.js
+++ functions/src/sessionCounter.js
@@ -5,12 +5,14 @@ module.exports = functions.database
   .onWrite(async (change) => {
 
     const collectionRef = change.after.ref.parent;
-    const countRef = collectionRef.parent.child('session_count');
+    const counterRef = collectionRef.parent.child('session_count');
 
     // in this case it is better to recount on every write rather than
     // keeping an incremental counter because we will be limiting the
     // number of sessions user may have anyway. so this approach will
     // make the value of session_count more robust.
     const sessionsData = await collectionRef.once('value');
-    return await counterRef.set(sessionsData.numChildren());
+    await counterRef.set(sessionsData.numChildren());
+
+    return null;
   });
